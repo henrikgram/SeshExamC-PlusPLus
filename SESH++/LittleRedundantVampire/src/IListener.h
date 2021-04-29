1fe831b1602f8dc03406cd29072208f675da12f0
GitGutAtGitHub <58419128+GitGutAtGitHub@users.noreply.github.com>
2021-04-29 10:27:41 +0200

    Revert "Merge branch 'BasicPlayer(StinnaEmma)'"
    This reverts commit f30a26f823042ed2aa00c5167a8c01dd0019d21b, reversing
    changes made to 351f33b0900cc9456b5faabb772c2fc25a30efd9.

--- SESH++/LittleRedundantVampire/src/IListener.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class IListener
-{
-public:
-	//TODO: Fix when merging with signe and henriks componentcode
-	virtual void Notify(std::string eventName/*, Component component*/) = 0;
-};
