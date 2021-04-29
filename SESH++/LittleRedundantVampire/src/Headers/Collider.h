954a2182bbe15abed9f2618b261770b3ed8f2b5f
GitGutAtGitHub <58419128+GitGutAtGitHub@users.noreply.github.com>
2021-04-29 10:41:25 +0200

    ManualMergeWithStinnaAndEmma

---
+++ SESH++/LittleRedundantVampire/src/Headers/Collider.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "CollisionEvent.h"
+
+using namespace sf;
+
+class Collider : protected IListener
+{
+public:
+	Collider(RectangleShape& body);
+	~Collider();
+
+	void Move(float deltaX, float deltaY) const { body.move(deltaX, deltaY); }
+
+	bool CheckCollision(const Collider& other, float pushFactor);
+	Vector2f GetPosition() const { return body.getPosition(); }
+	Vector2f GetHalfsize() const { return body.getSize() / 2.0f; }
+	void Notify(std::string eventName/*, Component component*/);
+
+private:
+	RectangleShape& body;
+	CollisionEvent onColliding;
+};
+
